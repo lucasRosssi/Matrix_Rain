66287a74fe4f1d1e2cf35e64cbeb8c6d7d145dc5
lucasRosssi <lucasrm2305@gmail.com>
2022-01-03 09:27:39 -0300

    created const in js file

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 const canvas = document.getElementById('Matrix')
+const context = canvas.getContext('2d')
+
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+
+const katakana = 'アァカサタナハマヤャラワガザダバパイィキシチニヒミリヰギジヂビピウゥクスツヌフムユュルグズブヅプエェケセテネヘメレヱゲゼデベペオォコソトノホモヨョロヲゴゾドボポヴッン'
+const latin = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+const nums = '0123456789'
+
+const alphabet = katakana + latin + nums
+
+const fontSize = 16
+const columns = canvas.width / fontSize
+
+const rainDrops = []
+
+for (let x = 0; x < columns; x++) {
+    rainDrops[x] = 1
+}
